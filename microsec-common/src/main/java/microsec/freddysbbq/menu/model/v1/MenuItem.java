4d51124f003b2f2a87a436f733cff5c3620357ab
pivotal <wtran@pivotal.io>
2015-09-15 18:17:22 -0400

    Menu resource server using spring-data-rest and spring-data-jpa.
    No security at the moment.

--- microsec-common/src/main/java/microsec/freddysbbq/menu/model/v1/MenuItem.java
+++ microsec-common/src/main/java/microsec/freddysbbq/menu/model/v1/MenuItem.java
@@ -2,11 +2,19 @@ package microsec.freddysbbq.menu.model.v1;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class MenuItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
     private BigDecimal price;
