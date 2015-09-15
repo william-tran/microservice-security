945aed0ac19aec09c1c99e0cec58b2c592a61500
pivotal <wtran@pivotal.io>
2015-09-15 17:54:30 -0400

    Add security testing magic. Test for https and oauth redirect.
    See tests fail.

---
+++ microsec-customer-portal/src/test/java/microsec/freddysbbq/customer_app/CustomerApplicationSecurityTests.java
@@ -0,0 +1,28 @@
+package microsec.freddysbbq.customer_app;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoProperties;
+import org.springframework.test.context.TestPropertySource;
+
+import microsec.test.SecurityIntegrationTest;
+
+@SpringApplicationConfiguration(classes = CustomerApplication.class)
+@TestPropertySource(properties = "security.require-ssl=true")
+public class CustomerApplicationSecurityTests extends SecurityIntegrationTest {
+
+    @Autowired
+    private OAuth2SsoProperties ssoProperties;
+
+    @Test
+    public void testHomepageSecurity() throws Exception {
+        checkRequiresHttpsAndOauthSso("/", ssoProperties.getLoginPath());
+    }
+
+    @Test
+    public void testMenuSecurity() throws Exception {
+        checkRequiresHttpsAndOauthSso("/menu", ssoProperties.getLoginPath());
+    }
+
+}
