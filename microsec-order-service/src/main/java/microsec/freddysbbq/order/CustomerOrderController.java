3d4fd5a7f502a86922f5b3103146233af07d943d
pivotal <wtran@pivotal.io>
2015-09-15 23:46:13 -0400

    Endpoints for customers to view and place their orders.
    Placed orders are a simple map
    of {menuItemId:quantity}, actual orders are hydrated using /userinfo and
    making calls to the MenuApplication resource server.
    
    This approach uses the OAuth2RestTemplate to relay the current token to
    the MenuApplication; this way the request to MenuApplication is made as
    the current user. Resource server token relay is activated by a
    non-blank spring.oauth2.client.clientId (even though it's not used).
    Another approach would be for the OrderApplication to obtain its own
    token as itself via client_credentials grant. OrderApplication would
    need a client with an authorized grant type of client_credentials and
    authority of menu.read, but the MenuApplication would not know who the
    user that originated the request is.

---
+++ microsec-order-service/src/main/java/microsec/freddysbbq/order/CustomerOrderController.java
@@ -0,0 +1,95 @@
+package microsec.freddysbbq.order;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import microsec.freddysbbq.menu.model.v1.MenuItem;
+import microsec.freddysbbq.order.model.v1.Order;
+import microsec.freddysbbq.order.model.v1.OrderItem;
+import microsec.uaa.model.v2.UserInfo;
+
+/**
+ * An Order controller for customer use
+ * 
+ * @author will.tran
+ *
+ */
+@RestController
+public class CustomerOrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OAuth2RestTemplate oAuth2RestTemplate;
+
+    @Autowired
+    private ResourceServerProperties resourceServerProperties;
+
+    @RequestMapping("/myorders")
+    public Collection<Order> getMyOrders(Principal principal) {
+        return orderRepository.findByCustomerId(principal.getName());
+    }
+
+    @RequestMapping(value = "/myorders", method = RequestMethod.POST)
+    public ResponseEntity<Void> placeOrder(Principal principal, @RequestBody Map<Long, Integer> itemsAndQuantites) {
+        if (itemsAndQuantites.isEmpty()) {
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+        UserInfo userInfo = oAuth2RestTemplate.getForObject(resourceServerProperties.getUserInfoUri(), UserInfo.class);
+        Order order = new Order();
+        order.setCustomerId(principal.getName());
+        order.setEmail(userInfo.getEmail());
+        order.setFirstName(userInfo.getGivenName());
+        order.setLastName(userInfo.getFamilyName());
+        order.setPlaced(new Date());
+        Set<OrderItem> items = new LinkedHashSet<>();
+        BigDecimal total = new BigDecimal(0);
+        for (Entry<Long, Integer> itemAndQuantity : itemsAndQuantites.entrySet()) {
+            Long itemId = itemAndQuantity.getKey();
+            Integer quantity = itemAndQuantity.getValue();
+            if (itemId == null || quantity != null && quantity < 0) {
+                return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+            } else if (quantity == null || quantity == 0) {
+                continue;
+            }
+            try {
+                MenuItem item = oAuth2RestTemplate
+                        .getForObject("http://localhost:8083/menuItems/{id}", MenuItem.class, itemId);
+                OrderItem orderItem = new OrderItem();
+                orderItem.setOrder(order);
+                orderItem.setMenuItemId(itemId);
+                orderItem.setName(item.getName());
+                orderItem.setPrice(item.getPrice());
+                orderItem.setQuantity(quantity);
+                items.add(orderItem);
+                total = total.add(item.getPrice().multiply(new BigDecimal(quantity)));
+            } catch (HttpClientErrorException e) {
+                if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                    return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+                }
+            }
+        }
+        order.setTotal(total);
+        order.setOrderItems(items);
+        orderRepository.save(order);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+}
